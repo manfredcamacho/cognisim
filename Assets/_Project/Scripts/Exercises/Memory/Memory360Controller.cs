b3bdf48eb3c7445eae77041e9ceecfa0878f7178
manfredcamacho <manfredcb@gmail.com>
2025-11-13 03:08:40 -0300

    Add seek feature with instructions

--- Assets/_Project/Scripts/Exercises/Memory/Memory360Controller.cs
+++ Assets/_Project/Scripts/Exercises/Memory/Memory360Controller.cs
@@ -2,32 +2,63 @@ using UnityEngine;
 using System.Collections.Generic;
 using System;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class Memory360Controller : MonoBehaviour, IExerciseController
 {
     // --- Inspector Fields ---
     [SerializeField] private CameraView360 cameraMouse; // El script que creaste para mover la cámara
-    
+    [SerializeField] private RayPointer rayPointer; // El script para el puntero de rayo
+
+    [SerializeField] private FeedbackToast feedbackToast; // Para mostrar mensajes de feedback
+
+    [SerializeField] private List<GameObject> dialogsToShow; // Diálogos de instrucciones y cierre
+
     // --- IExerciseController ---
     public event Action OnExerciseComplete;
     
     // --- Private Fields ---
     private string exerciseId;
-    private List<string> targetsToFind;
+    public List<string> targetsToFind;
     private List<string> targetsFound;
+    private int currentDialogIndex = 0;
     private bool exerciseIsActive = false;
+
+    private void Start()
+    {
+        Debug.Log("Memory360Controller started.");
+        // Mostrar el primer diálogo de instrucciones al inicio
+        if (dialogsToShow.Count > 0)
+        {
+            dialogsToShow[0].SetActive(true);
+        }
+    }
 
     // El diálogo de inicio llamará a este método cuando el usuario presione "Iniciar"
     public void StartExercise()
     {
+        Debug.Log("Memory360Controller: Starting exercise.");
+        dialogsToShow[currentDialogIndex].SetActive(false);
         exerciseIsActive = true;
         cameraMouse.enabled = true;
+        rayPointer.enabled = true;
         MetricsManager.Instance.LogEvent(exerciseId, "ExerciseStart", null, $"Targets: {string.Join(", ", targetsToFind)}");
+    }
+
+    public void nextDialog()
+    {
+        Debug.Log("Memory360Controller: Moving to next dialog.");
+
+        dialogsToShow[currentDialogIndex].SetActive(false);
+        currentDialogIndex++;
+        dialogsToShow[currentDialogIndex].SetActive(true);
     }
 
     // El SessionController llama a este método PRIMERO
     public void Initialize(string exerciseId, ExerciseParameters parameters)
     {
+        Debug.Log("Memory360Controller: Initializing exercise.");
+
         this.exerciseId = exerciseId;
         targetsToFind = new List<string>(parameters.targetObjects);
         targetsFound = new List<string>();
@@ -48,6 +79,8 @@ public class Memory360Controller : MonoBehaviour, IExerciseController
     /// </summary>
     private void HandleObjectClicked(InteractableObject360 clickedObject)
     {
+        Debug.Log("Memory360Controller: Object clicked: " + clickedObject.objectId);
+
         if (!exerciseIsActive) return; // No hacer nada si el ejercicio no ha comenzado
 
         string id = clickedObject.objectId;
@@ -91,6 +124,8 @@ public class Memory360Controller : MonoBehaviour, IExerciseController
 
     private void EndExercise(string reason)
     {
+        Debug.Log("Memory360Controller: Ending exercise. Reason: " + reason);
+
         if (!exerciseIsActive) return;
         exerciseIsActive = false;
 
